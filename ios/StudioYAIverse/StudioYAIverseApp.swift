b7e77f972bd7e1994016c8fc7f6ae4b902e98ab1
kdha0727 <dhakim@yonsei.ac.kr>
2023-05-10 02:42:17 +0900

    ios automatically-generated files

---
+++ ios/StudioYAIverse/StudioYAIverseApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudioYAIverseApp.swift
+//  StudioYAIverse
+//
+//  Created by Dongha kim on 2023/05/10.
+//
+
+import SwiftUI
+
+@main
+struct StudioYAIverseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
